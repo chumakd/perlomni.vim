a6ed733864d257368e3cffeb97a7c59ba5edc12b
Daniel Vinciguerra <d2@leismunicipais.com.br>
2015-07-06 21:47:29 -0300

    fix perlapi script

--- utils/perl_api_functions.pl
+++ utils/perl_api_functions.pl
@@ -1,26 +1,39 @@
 #!/usr/bin/env perl
-# get perl function list
-my $pathes = join ' ',@INC;
-# my $pod = qx( find $pathes -name );
-# chomp $pod;
+use strict;
+use feature ':5.10';
+use warnings;
 
-open(FH, '-|', qq|podselect -section 'DESCRIPTION/*' /opt/local/lib/perl5/5.10.1/pods/perlapi.pod| );
-my @func ;
-my $inline = 0;
-while( <FH> )
-{
-    if( /^=over/ ) {
-        $inlist++;
+
+# getting perlapi.pod absolute path
+my $filepath;
+for my $path ( @INC ) {
+    $filepath = "$path/pod/perlapi.pod"
+        if -e "$path/pod/perlapi.pod";
+}
+
+
+my ( $functions, $inlist, $file ) = ( [], 0, []);
+
+# open file and get content
+open my $fh, '-|', "podselect -section 'DESCRIPTION/*' $filepath";
+@$file = map { chomp; $_ } <$fh>;
+close $fh;
+
+
+# parse perlapi file
+foreach (@$file){
+    if ( /^=over/ ) {
+        $inlist++
     }
-    elsif( /^=back/ ) {
+    elsif ( /^=back/ ) {
         $inlist--;
     }
-    elsif( /^=item \w+/ ) {
-        s/^=item //;
-        chomp;
-        push @func,$_ if $inlist == 1;
+    elsif ( /^=item \w+/ ) {
+        s/^=item //; chomp;
+        push @$functions, $_ if $inlist == 1;
     }
 }
-close FH;
 
-print $_ , "\n" for @func;
+# output perl api functions
+say $_ for @$functions;
+
