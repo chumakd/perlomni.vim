7cc7c9de5997a0ae278c7ad74a3516b09b42b354
eddie <eddie@ashfamily.net>
2016-02-13 21:04:21 -0500

    comp with alias working

--- rplugin/python3/deoplete/sources/perlomni2.py
+++ rplugin/python3/deoplete/sources/perlomni2.py
@@ -1,4 +1,4 @@
-#min pattern length 1
+# min pattern length 1
 from .perl_base import Perl_base
 # import deoplete.util
 import time
@@ -34,15 +34,56 @@ class Source(Perl_base):
                 'comp': self.CompMooseIsa,
             },
             {
-                'only':1,
-                'head': '^has\s+\w+' ,
-                'context': '^\s*$' ,
+                'only': 1,
+                'head': '^has\s+\w+',
+                'context': '^\s*$',
                 'backward': '\w*$',
                 'comp': self.CompMooseAttribute,
             },
-
-        ]
-
+            {
+                'only': 1,
+                'head': '^with\s+',
+                'context': '^\s*-$',
+                'backward': '\w+$',
+                'comp': ['alias', 'excludes'],
+            },
+            # {
+            #     'context': '^\s*$',
+            #     'backward': '\w+$',
+            #     'comp': [
+            #         'extends' , 'after' , 'before', 'has' ,
+            #         'requires' , 'with' , 'override' , 'method',
+            #         'super', 'around', 'inner', 'augment', 'confess' , 'blessed' ]
+            #
+            # },
+            # {
+            #     'only': 1,
+            #     'context': '^use\s+[a-zA-Z0-9:]+\s+qw',
+            #     'backward': '\w*$',
+            #     'comp': self.CompExportFunction,
+            # },
+            # {
+            #     'only': 1,
+            #     # TODO fix <,word boundary
+            #     # 'context': '\<\(new\|use\)\s\+\(\(base\|parent\)\s\+\(qw\)\?[''"(/]\)\?$' ,
+            #     # 'backward': '\<[A-Z][A-Za-z0-9_:]*$',
+            #     'context': '\b(new|use)\s+(base|parent)\s+(qw)?[\'"\(/])?$',
+            #     'backward': '\b\w[\w\d_:]*$',
+            #     'comp': self.CompClassName,
+            # },
+            # {
+            #     'only': 1,
+            #     'context': '^\s*extends\s+[\'"]$',
+            #     'backward': '[\w\d_:]+$',
+            #     'comp': self.CompClassName,
+            # },
+            # {
+            #     'only': 1,
+            #     'context': '^\s*(sub|method)\s+',
+            #     'backward': '\w+$',
+            #     'comp': self.CompCurrentBaseFunction,
+            # },
+            ]
 
     def gather_candidates(self, context):
         values = self.PerlComplete(0)
@@ -79,20 +120,20 @@ class Source(Perl_base):
             expiry = self._perlomni_cache_expiry
             last_ts = self._last_cache_ts
 
-        #todo error - unsupported operand
+        # TODO error - unsupported operand
         if time.localtime - last_ts > expiry:
             if key in self._cache_expiry:
                 self._cache_last[key] = time.localtime
             else:
                 self._last_cache_ts = time.localtime()
-                return 0
+                return
 
         if self.vim.eval('echo g:perlomni_use_cache || 1') == 1:
-            return 0
+            return
 
         if key in self._cache[key]:
             return self._cache[key]
-        return 0
+        return
 
     def SetCacheNSWithExpiry(self, ns, cache_key, value, exp):
         key = ns + '_' + cache_key
@@ -121,7 +162,8 @@ class Source(Perl_base):
     def scanClass(self, path):
         cache = self.GetCacheNS('classpath', path)
         lib = []
-        if isinstance(cache, list):
+        if cache is not None:
+
             return cache
 
         # TODO
@@ -138,9 +180,26 @@ class Source(Perl_base):
                         lib.append(root+name)
                         return self.SetCacheNS('classpath', path, files)
 
-    # TODO
-    def StringFilter(self):
-        return
+    #" scan exported functions from a module.
+    def scanModuleExportFunctions(self, mClass):
+        cache = self.GetCacheNS('mef', mClass)
+        if cache is not None:
+            return cache
+        funcs = []
+
+        # TODO: TOO SLOW, CACHE TO FILE!!!!
+        if self.vim.eval('exists("g:perlomni_export_functions")'):
+            output = self.runPerlEval(
+                mClass,
+                'print join " ",@' +
+                mClass +
+                '::EXPORT_OK,@' +
+                mClass +
+                '::EXPORT')
+            funcs = output.split()
+        return self.SetCacheNS(
+            'mef', mClass, self.toCompHashList(
+                funcs, mClass))
 
     def CompClassName(self, base, context):
         self.debug('In CompClassName')
@@ -170,7 +229,6 @@ class Source(Perl_base):
     def parseParagraphHead(self, fromLine):
         lnum = fromLine
         paragraph_head = self.vim.current.buffer[lnum]
-        # TODO for
         for nr in range(lnum-1, lnum-10, -1):
             line = self.vim.current.buffer[nr]
             if re.match('^\s*$', line) or re.match('^\s*#', line):
@@ -180,24 +238,90 @@ class Source(Perl_base):
 
     def CompMooseIsa(self, base, context):
         comps = ['Int', 'Str', 'HashRef', 'HashRef[', 'Num', 'ArrayRef']
-        #TODO
+        # TODO
         # comps += self.CompClassName(base, context)
         ret = []
         for i in comps:
             ret.append("'"+i+"'")
         return ret
 
-
-
-    def CompMooseAttribute(self,base,context):
-        values = [ 'default' , 'is' , 'isa' ,
-                     'label' , 'predicate', 'metaclass', 'label',
-                     'expires_after',
-                     'refresh_with' , 'required' , 'coerce' , 'does' , 'required',
-                     'weak_ref' , 'lazy' , 'auto_deref' , 'trigger',
-                     'handles' , 'traits' , 'builder' , 'clearer',
-                     'predicate' , 'lazy_build', 'initializer', 'documentation' ]
+    def CompMooseAttribute(self, base, context):
+        values = ['default', 'is', 'isa',
+                  'label', 'predicate', 'metaclass', 'label',
+                  'expires_after',
+                  'refresh_with', 'required', 'coerce', 'does', 'required',
+                  'weak_ref', 'lazy', 'auto_deref', 'trigger',
+                  'handles', 'traits', 'builder', 'clearer',
+                  'predicate', 'lazy_build', 'initializer', 'documentation']
         ret = []
         for i in values:
             ret.append(i+' => ')
         return ret
+
+    def CompExportFunction(self, base, context):
+        mod_pattern = '[a-zA-Z][a-zA-Z0-9:]\+'
+        m = re.match('\(^use\s\+\)\@<=' + mod_pattern, context)
+        funcs = self.toCompHashList(
+            self.scanModuleExportFunctions(
+                m.group(0)), m.group(0))
+        return funcs
+
+    def CompCurrentBaseFunction(self, base, context):
+        all_mods = self.findCurrentClassBaseClass()
+        funcs = []
+        for mod in all_mods:
+            sublist = self.scanFunctionFromClass(mod)
+            funcs += sublist
+        return funcs
+
+    # TODO
+    def runPerlEval(self, Mclass, string):
+        return
+
+    #" util function for building completion hashlist
+    def toCompHashList(self, mList, menu):
+        # return map( a:list , '{ "word": v:val , "menu": "'. a:menu .'" }' )
+        # TODO verify this works
+        return [{'word': val, 'menu': menu} for val in mList]
+
+    def findCurrentClassBaseClass(self):
+        all_mods = []
+        for i in range(self.vim.current.line, 0, -1):
+            line = self.vim.current.buffer[i]
+            if re.match('^package\s+', line):
+                break
+            match = re.match(
+                '^(?:use\s+(?:base|parent)|extends)\s+(.*)\s*;', line)
+            if match:
+                res = match.group(0)
+                match = re.match('^qw[\'"\(\[](.*).$', res)
+                args = match.group(0)
+                all_mods += args.split('\s+', args)
+        return all_mods
+
+    def scanFunctionFromClass(self, mClass):
+        classfile = self.locateClassFile(mClass)
+        if classfile is None:
+            return []
+        return self.scanFunctionFromSingleClassFile(
+            classfile) + self.scanFunctionFromBaseClassFile(classfile)
+
+    def locateClassFile(self, mClass):
+        cache = self.GetCacheNS('clsfpath', mClass)
+        if cache is not None:
+            return cache
+
+        paths = self.vim.options['path'].split(',')
+        # TODO
+        # if g:perlomni_use_perlinc || &filetype != 'perl'
+        # TODO
+        # paths = split( s:system(g:perlomni_perl, '-e', 'print join(",",@INC)')
+        # ,',')
+        filepath = mClass+'.pm'
+        filepath.replace('/', '::')
+
+        paths += 'lib'
+        for path in paths:
+            if os.path.isfile(path + '/' + filepath):
+                return self.SetCacheNS('clsfpath', mClass, path+'/' + filepath)
+        return
